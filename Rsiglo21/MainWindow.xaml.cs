860b8e00dacbc1844a4c572fa0ec99a065849b5e
LonelyGoblin <josuegf.19@gmail.com>
2022-09-05 23:18:25 -0300

    Version conectada a la bdd

--- Rsiglo21/MainWindow.xaml.cs
+++ Rsiglo21/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OracleClient;
+using Oracle;
+
 
 namespace Rsiglo21
 {
@@ -20,9 +26,53 @@ namespace Rsiglo21
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string con = "DATA SOURCE = (DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=localhost)(Port=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl))); " +
+           "PASSWORD=admin; USER ID= principal;";
         public MainWindow()
         {
             InitializeComponent();
+            conexion();
+            cargarDT();
+        }
+
+        public void conexion()
+        {
+            OracleConnection ora = new OracleConnection(con);
+            ora.Open();
+
+            ora.Close();
+
+        }
+        private void cargarDT()
+        {
+            OracleConnection ora = new OracleConnection(con);
+           // OracleCommand cmd = new OracleCommand("select * from cliente", ora);
+            //ora.Open();            
+            //cmd.CommandType = CommandType.Text;
+
+          
+            OracleCommand cmd = new OracleCommand("cargadatos", ora);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("registros", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+
+             OracleDataAdapter adaptador = new OracleDataAdapter();
+             adaptador.SelectCommand = cmd;
+             DataTable tabla = new DataTable();
+             adaptador.Fill(tabla);
+
+            dgclientes.ItemsSource = tabla.AsDataView();
+
+
+            ora.Close();
+        }
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void dgclientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- Rsiglo21/view/Login.xaml.cs
+++ Rsiglo21/view/Login.xaml.cs
@@ -11,17 +11,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.OracleClient;
+using Oracle;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Rsiglo21.view
 {
     /// <summary>
     /// Lógica de interacción para Login.xaml
-    /// </summary>
+    /// </summary> 
+   
     public partial class Login : Window
     {
+        public string con = "DATA SOURCE = (DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(Host=localhost)(Port=1521)))(CONNECT_DATA=(SERVICE_NAME=orcl))); " +
+           "PASSWORD=admin; USER ID= principal;";
         public Login()
         {
             InitializeComponent();
+            conexion();
+        }
+
+        public void conexion()
+        {
+            OracleConnection ora = new OracleConnection(con);
+            ora.Open();
+
+            ora.Close();
+
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
