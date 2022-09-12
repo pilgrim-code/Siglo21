7d843a1a9e8ccf6f5a2b2420ef50138fb407a079
pilgrim-code <luk.pina@duocuc.cl>
2022-09-12 04:25:38 -0300

    CRUD MESAS

--- Rsiglo21/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Rsiglo21/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace Rsiglo21 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Rsiglo21;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Rsiglo21;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
