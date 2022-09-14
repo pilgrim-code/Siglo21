e1c5cd4698de475f7369cb226798318b15f29806
pilgrim-code <luk.pina@duocuc.cl>
2022-09-14 05:42:09 -0300

    Editar mesas listocooo

--- Rsiglo21/view/Mesa.xaml.cs
+++ Rsiglo21/view/Mesa.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
 
 namespace Rsiglo21.view
 {
@@ -51,7 +52,7 @@ namespace Rsiglo21.view
                 OracleConnection ora = new OracleConnection(con);
 
                 ora.Open();
-                OracleCommand cmd = new OracleCommand("id_mesa", ora);
+                OracleCommand cmd = new OracleCommand("CARGADATOS", ora);
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
                 cmd.Parameters.Add("mesa", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
                 OracleDataAdapter adaptador = new OracleDataAdapter();
@@ -83,36 +84,41 @@ namespace Rsiglo21.view
             cmd.ExecuteNonQuery();
             ora.Close();
 
-           
-                //cmd.CommandType = System.Data.CommandType.StoredProcedure;
-               // if (idM.Text.Length >= 1 && cPersonas.Text.Length >= 1)
-                //{
-                  /*  cmd.Parameters.Add("id_m", OracleDbType.Int32,ParameterDirection.Input).Value = Int32.Parse(idM.Text);
-                    cmd.Parameters.Add("cant_p", OracleDbType.Int32, ParameterDirection.Input).Value = Int32.Parse(cPersonas.Text);
-                    cmd.Parameters.Add("v_estado", OracleDbType.Int32, ParameterDirection.Input).Value = Int32.Parse("d");
-                  */
-                    MessageBox.Show("Mesa añadida");
-               // }
-               // else
-                //{
-                    //MessageBox.Show("Ingrese ID  Y cantidad de personas");
-               // }
+
+
+            MessageBox.Show("Mesa añadida");
+            cargarDT();
+
+
 
 
 
             //}
-           /* catch (Exception)
-            {
+            /* catch (Exception)
+             {
 
-                MessageBox.Show("Error");
+                 MessageBox.Show("Error");
 
-            }*/
+             }*/
             ora.Close();
         }
 
         private void actualizarMesa(object sender, RoutedEventArgs e)
         {
-           cargarDT();
+           OracleConnection ora = new OracleConnection(con);
+
+            ora.Open();
+            OracleCommand cmd = new OracleCommand("EDITAR_MESA", ora);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("vid_m", OracleDbType.Int32, Int32.Parse(idM.Text), ParameterDirection.Input);
+            cmd.Parameters.Add("vcant_p", OracleDbType.Int32,Int32.Parse(cPersonas.Text), ParameterDirection.Input);
+            cmd.Parameters.Add("v_estado", OracleDbType.Char, 'd', ParameterDirection.Input);
+           cmd.ExecuteNonQuery();
+            MessageBox.Show("MESA ACTUALIZADA");
+            cargarDT();
+            ora.Close();
+
+
         }
 
         private void EliminarM(object sender, RoutedEventArgs e)
@@ -130,24 +136,18 @@ namespace Rsiglo21.view
 
             cmd.ExecuteNonQuery();
             MessageBox.Show("Mesa Eliminada");
+            cargarDT();
             ora.Close();
 
            
-          /*
-            if (idM.Text.Length >= 1)
-            {
-                cmd.Parameters.Add("id_m", OracleType.Number).Value = Convert.ToInt32(idM.Text);
-               
-                ora.Close();
-            }
-            else
-            {
-                MessageBox.Show("Ingrese ID Valido");
-                ora.Close();
-            }
-          */
+        
 
 
         }
+
+        
+           
+
+
     }
 }
